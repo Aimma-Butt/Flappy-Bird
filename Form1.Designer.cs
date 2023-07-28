de8fc0b1a10ad4f993720c6a38065aca55acf972
Aimma Butt <104351004+Aimma-Butt@users.noreply.github.com>
2023-07-28 10:16:22 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace HCI_LAB_PROJECT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.t1 = new System.Windows.Forms.PictureBox();
+            this.t2 = new System.Windows.Forms.PictureBox();
+            this.t3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.h2 = new System.Windows.Forms.PictureBox();
+            this.h4 = new System.Windows.Forms.PictureBox();
+            this.h3 = new System.Windows.Forms.PictureBox();
+            this.h1 = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.t1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.label1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // t1
+            // 
+            this.t1.BackColor = System.Drawing.Color.Transparent;
+            this.t1.Image = ((System.Drawing.Image)(resources.GetObject("t1.Image")));
+            this.t1.Location = new System.Drawing.Point(-2, 312);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(91, 89);
+            this.t1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.t1.TabIndex = 0;
+            this.t1.TabStop = false;
+            // 
+            // t2
+            // 
+            this.t2.BackColor = System.Drawing.Color.Transparent;
+            this.t2.Image = ((System.Drawing.Image)(resources.GetObject("t2.Image")));
+            this.t2.Location = new System.Drawing.Point(303, 345);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(68, 56);
+            this.t2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.t2.TabIndex = 1;
+            this.t2.TabStop = false;
+            // 
+            // t3
+            // 
+            this.t3.BackColor = System.Drawing.Color.Transparent;
+            this.t3.Image = ((System.Drawing.Image)(resources.GetObject("t3.Image")));
+            this.t3.Location = new System.Drawing.Point(595, 312);
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(83, 89);
+            this.t3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.t3.TabIndex = 2;
+            this.t3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // h2
+            // 
+            this.h2.BackColor = System.Drawing.Color.Transparent;
+            this.h2.Image = ((System.Drawing.Image)(resources.GetObject("h2.Image")));
+            this.h2.Location = new System.Drawing.Point(133, 229);
+            this.h2.Name = "h2";
+            this.h2.Size = new System.Drawing.Size(67, 239);
+            this.h2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.h2.TabIndex = 3;
+            this.h2.TabStop = false;
+            // 
+            // h4
+            // 
+            this.h4.BackColor = System.Drawing.Color.Transparent;
+            this.h4.Image = ((System.Drawing.Image)(resources.GetObject("h4.Image")));
+            this.h4.Location = new System.Drawing.Point(481, 291);
+            this.h4.Name = "h4";
+            this.h4.Size = new System.Drawing.Size(67, 177);
+            this.h4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.h4.TabIndex = 4;
+            this.h4.TabStop = false;
+            // 
+            // h3
+            // 
+            this.h3.BackColor = System.Drawing.Color.Transparent;
+            this.h3.Image = ((System.Drawing.Image)(resources.GetObject("h3.Image")));
+            this.h3.Location = new System.Drawing.Point(481, 1);
+            this.h3.Name = "h3";
+            this.h3.Size = new System.Drawing.Size(67, 114);
+            this.h3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.h3.TabIndex = 5;
+            this.h3.TabStop = false;
+            // 
+            // h1
+            // 
+            this.h1.BackColor = System.Drawing.Color.Transparent;
+            this.h1.Image = ((System.Drawing.Image)(resources.GetObject("h1.Image")));
+            this.h1.Location = new System.Drawing.Point(133, 1);
+            this.h1.Name = "h1";
+            this.h1.Size = new System.Drawing.Size(67, 114);
+            this.h1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.h1.TabIndex = 6;
+            this.h1.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.BackColor = System.Drawing.Color.Transparent;
+            this.bird.Image = ((System.Drawing.Image)(resources.GetObject("bird.Image")));
+            this.bird.Location = new System.Drawing.Point(43, 157);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(46, 46);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 7;
+            this.bird.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(324, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 91);
+            this.label1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.label1.TabIndex = 10;
+            this.label1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(787, 493);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.h1);
+            this.Controls.Add(this.h3);
+            this.Controls.Add(this.h4);
+            this.Controls.Add(this.h2);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.t1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.label1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox t1;
+        private System.Windows.Forms.PictureBox t2;
+        private System.Windows.Forms.PictureBox t3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox h2;
+        private System.Windows.Forms.PictureBox h4;
+        private System.Windows.Forms.PictureBox h3;
+        private System.Windows.Forms.PictureBox h1;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HCI_LAB_PROJECT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            label1.Visible = false; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            movetree(15); // move trees
+            movehurdle(10); // move hurdles
+            gameover(); 
+        }
+
+        Random r = new Random(); 
+        void movehurdle(int speed)
+        {
+            if (h1.Left >= 0)
+            {
+                h1.Left += -speed;
+            }
+            else
+            {
+                h1.Left = 1020;
+                h1.Height = r.Next(100, 200);  //it will randomly select the value b/w
+                //size of hurdle change  
+
+            }
+
+            if (h2.Left >= 0)
+            {
+                h2.Left += -speed;
+            }
+            else
+            {
+                h2.Left = 1020;
+                h2.Height = r.Next(100, 400); 
+            }
+
+            if (h3.Left >= 0)
+            {
+                h3.Left += -speed;
+            }
+            else
+            {
+                h3.Left = 1020;
+                h3.Height = r.Next(100, 300); 
+            }
+
+            if (h4.Left >= 0)
+            {
+                h4.Left += -speed;
+            }
+            else
+            {
+                h4.Left = 1020;
+                h4.Height = r.Next(100, 200);
+            }
+        }
+
+
+
+        //int speed will determine the speed of moving tree
+        void movetree(int speed) //function to move trees
+        {
+            if (t1.Left >= 0)
+            {
+                t1.Left += -speed;
+            }
+            else
+            {
+                t1.Left = 1020;
+            }
+
+            if (t2.Left >= 0)
+            {
+                t2.Left += -speed;
+            }
+            else
+            {
+                t2.Left = 1020;
+            }
+
+            if (t3.Left >= 0)
+            {
+                t3.Left += -speed;
+            }
+            else
+            {
+                t3.Left = 1020;
+            }
+
+        }
+
+        int score = 0; 
+
+        void gameover()
+        {
+            if ((bird.Bounds.IntersectsWith(h1.Bounds)) || (bird.Bounds.IntersectsWith(h2.Bounds)) || (bird.Bounds.IntersectsWith(h3.Bounds)) || (bird.Bounds.IntersectsWith(h4.Bounds)))
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                label1.Visible = true; 
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(bird.Top<=440)
+            bird.Top += 2;
+            score++; 
+            label2.Text = score.ToString(); 
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) 
+            {
+                bird.Top += -40; 
+            }
+        }
+
+
+    }
+}
